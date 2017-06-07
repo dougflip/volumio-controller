f2ca106514eb4bec0a5457e4f062f4570a12b684
Doug DiFilippo <ddifilippo@turnitin.com>
2017-06-06 23:30:38 -0400

    Play when button is pressed

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 const cp = require('child_process');
 const socket = require('socket.io-client')('http://localhost:3000');
+const Button = require('gpio-button');
 
 const LIGHT_PIN = 2;
-
 const turnLightOn = () => cp.execSync(`echo "1" > /sys/class/gpio/gpio2/value`);
 const turnLightOff = () => cp.execSync(`echo "0" > /sys/class/gpio/gpio2/value`);
+
+const button4 = new Button('button4');
+
+// maintain the latest piece of state so we can toggle play/pause via button press
+let currentVolumioState = {};
 
 socket.on('connect', function() {
 	console.log('=============');
 	console.log('connected!');
 	console.log('=============');
+
+	button4.on('press', function() {
+		const cmd = currentVolumioState.status && currentVolumioState.status === 'play'
+			? 'pause'
+			: 'play';
+		socket.emit(cmd);
+	});
 });
 
 socket.on('pushState', data => {
@@ -21,4 +33,5 @@ socket.on('pushState', data => {
 	console.log('=============');
 
 	cp.execSync(`echo ${newValue} > /sys/class/gpio/gpio2/value`);
+	currentVolumioState = data;
 });
